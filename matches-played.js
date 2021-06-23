6c814c3a64360cc29354b6fcccc6b5bc525e5a16
Muaaz <techbrothers3@gmail.com>
2021-06-23 14:47:45 +0530

    add logic for matches-played

---
+++ matches-played.js
@@ -0,0 +1,24 @@
+const csvtojson = require('csvtojson');
+const csvfilepath = "/home/mohammed/Documents/muaaz/data-project/archive/matches.csv";
+csvtojson()
+.fromFile(csvfilepath)
+.then((json) =>{
+    // console.log(json)
+    console.log(myFunction(json));
+})
+function myFunction(json) {
+    let map = {};
+    for(let i = 0; i<json.length; i++) {
+        if (map[json[i].season] != undefined) {
+            var count = map[json[i].season];
+            count++;    
+            map[json[i].season] = count;
+        } else {
+            map[json[i].season] = 1;
+        }
+    }
+    
+    return map;
+}
+
+
---
+++ sample.js
@@ -0,0 +1,8 @@
+let map = {
+    season : 3
+
+}
+
+console.log(map.season);
+map.season = 4;
+console.log(map.season);
