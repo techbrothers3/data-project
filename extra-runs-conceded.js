f78896f45e16db0f9e7cf3134fa27b24abd0ca9b
Muaaz <techbrothers3@gmail.com>
2021-06-23 16:19:44 +0530

    add logic for extra runs conceded

---
+++ extra-runs-conceded.js
@@ -0,0 +1,26 @@
+const csvtojson = require('csvtojson');
+const csvfilepath = "/home/mohammed/Documents/muaaz/data-project/archive/deliveries.csv";
+csvtojson()
+.fromFile(csvfilepath)
+.then((json) =>{
+    // console.log(json)
+    console.log(myFunction(json));
+})
+function myFunction(json) {
+    let map = {};
+    for(var i = 0; i<json.length; i++) {
+        if (i >= 136355) {
+            for(i; i<json.length; i++) {
+                if(map[json[i].bowling_team] != undefined) {
+                    var count = map[json[i].bowling_team];
+                    count += parseInt(json[i].extra_runs);
+                    map[json[i].bowling_team] = count;
+                } else {
+                    map[json[i].bowling_team] = parseInt(json[i].extra_runs);
+                    
+                }
+            }
+        }
+    }
+    return map;
+}
